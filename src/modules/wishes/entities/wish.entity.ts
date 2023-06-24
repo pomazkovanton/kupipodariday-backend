8a759fc5ca16d1eaf1a951cb99b245a6645a999d
pomazkovanton <vitrumbeta@gmail.com>
2023-06-24 22:36:46 +0800

    feat: описать схемы данных

--- src/modules/wishes/entities/wish.entity.ts
+++ src/modules/wishes/entities/wish.entity.ts
@@ -1,1 +1,59 @@
-export class Wish {}
+import { Column, Entity, ManyToOne, OneToMany } from 'typeorm';
+import {
+  IsUrl,
+  Length,
+  IsNumber,
+  IsPositive,
+  IsDecimal,
+} from 'class-validator';
+
+import { BaseEntity } from '../../../common/base-entity';
+import { User } from '../../users/entities/user.entity';
+import { Offer } from '../../../modules/offers/entities/offer.entity';
+import { Wishlist } from '../../../modules/wishlists/entities/wishlist.entity';
+
+@Entity()
+export class Wish extends BaseEntity {
+  @Column()
+  @Length(1, 250)
+  name: string;
+
+  @Column()
+  @IsUrl()
+  link: string;
+
+  @Column()
+  @IsUrl()
+  image: string;
+
+  @Column()
+  @IsNumber({
+    maxDecimalPlaces: 2,
+  })
+  @IsPositive()
+  price: number;
+
+  @Column()
+  @IsNumber({
+    maxDecimalPlaces: 2,
+  })
+  @IsPositive()
+  raised: number;
+
+  @Column()
+  @Length(1, 1024)
+  description: string;
+
+  @Column({ default: 0 })
+  @IsDecimal()
+  copied: number;
+
+  @ManyToOne(() => User, (user) => user.wishes)
+  owner: User;
+
+  @OneToMany(() => Offer, (offer) => offer.item)
+  offers: Offer[];
+
+  @ManyToOne(() => Wishlist, (wishlist) => wishlist.items)
+  wishlist: Wishlist;
+}
--- src/modules/wishlists/entities/wishlist.entity.ts
+++ src/modules/wishlists/entities/wishlist.entity.ts
@@ -1,1 +1,26 @@
-export class Wishlist {}
+import { Length, MaxLength } from 'class-validator';
+import { Column, Entity, ManyToOne, OneToMany } from 'typeorm';
+
+import { BaseEntity } from '../../../common/base-entity';
+import { User } from '../../../modules/users/entities/user.entity';
+import { Wish } from '../../../modules/wishes/entities/wish.entity';
+
+@Entity()
+export class Wishlist extends BaseEntity {
+  @Column()
+  @Length(1, 250)
+  name: string;
+
+  @Column()
+  @MaxLength(1500)
+  description: string;
+
+  @Column()
+  image: string;
+
+  @ManyToOne(() => User, (user) => user.wishlists)
+  owner: User;
+
+  @OneToMany(() => Wish, (wish) => wish.wishlist)
+  items: Wish[];
+}
