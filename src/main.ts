72434be709ff39843c9b01a05a1939559db93be6
pomazkovanton <vitrumbeta@gmail.com>
2023-07-04 19:19:59 +0800

    build: добавить логер

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app/app.module';
 import { ConfigService } from '@nestjs/config';
+import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
-  const port = configService.get('PORT') || 3000;
+  const port = configService.get('port') || 3000;
   app.enableCors();
+  app.useLogger(app.get(WINSTON_MODULE_NEST_PROVIDER));
   await app.listen(port);
 }
 bootstrap();
--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -1,6 +1,8 @@
+import { HashModule } from './../hash/hash.module';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
+import { WinstonModule } from 'nest-winston';
 
 import { DatabaseConfig } from '../../configs/database.config';
 import { config } from '../../configs/config';
@@ -12,6 +14,8 @@ import { WishesModule } from '../wishes/wishes.module';
 import { WishlistsModule } from '../wishlists/wishlists.module';
 import { OffersModule } from '../offers/offers.module';
 import { AuthModule } from '../auth/auth.module';
+import { loggerConfig } from 'src/configs/logger.config';
+
 @Module({
   imports: [
     ConfigModule.forRoot({
@@ -22,11 +26,13 @@ import { AuthModule } from '../auth/auth.module';
       imports: [ConfigModule],
       useClass: DatabaseConfig,
     }),
+    WinstonModule.forRoot(loggerConfig),
     UsersModule,
     WishesModule,
     WishlistsModule,
     OffersModule,
     AuthModule,
+    HashModule,
   ],
   controllers: [AppController],
   providers: [],
