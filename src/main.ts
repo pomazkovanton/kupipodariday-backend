c691319356c8859e9e6c0e6fe66cfc8b25d98fe6
pomazkovanton <vitrumbeta@gmail.com>
2023-07-06 14:22:32 +0800

    fix: добавить глобальную валидацию

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app/app.module';
 import { ConfigService } from '@nestjs/config';
 import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,6 +10,7 @@ async function bootstrap() {
   const port = configService.get('port') || 3000;
   app.enableCors();
   app.useLogger(app.get(WINSTON_MODULE_NEST_PROVIDER));
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   await app.listen(port);
 }
 bootstrap();
